b5c7e6766116b308bf11c7692241b1c58c5a81e3
Ethan Doan <ethandoan10@gmail.com>
2022-08-24 14:39:13 -0400

    header

--- portfolio/src/components/header/Header.jsx
+++ portfolio/src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Header.css'
 import CTA from './CTA'
-import ME from '../../assets/me-about.jpeg'
+import ME from '../../assets/me-about.png'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
